468ea92295c47dfe2f3fad305dd933c9ee78e166
Glen Mailer <glenjamin@gmail.com>
2015-05-10 22:33:06 +0100

    accept port config from env

--- webpack/server.js
+++ webpack/server.js
@@ -2,14 +2,16 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var port = process.env.PORT || 3000;
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
-}).listen(3000, function(err) {
+}).listen(port, function(err) {
   if (err) {
     throw err;
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:%d/', port);
 });
